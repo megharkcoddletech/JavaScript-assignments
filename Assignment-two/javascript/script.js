2740572e81645026e8353561765bd71ce6c8495d
Megha R K <meghasuryark@gmail.com>
2023-08-18 10:14:49 +0530

    assignment two correction is added

--- Assignment-two/javascript/script.js
+++ Assignment-two/javascript/script.js
@@ -1,20 +1,20 @@
 // 1)
 //dont use keyword names like array for naming variables
 
-var array = ['1', '2', '3', '4', '5', '6', '7'];
-array.splice(5, 1);
-console.log(array);
+let numbers = ['1', '2', '3', '4', '5', '6', '7'];
+numbers.splice(5, 1);
+console.log(numbers);
 
 // a).Remove number "6" from the array and console the length of the array.
 
-console.log('length of the array is ' + array.length);
+console.log('length of the array is ' + numbers.length);
 
 // b. Convert all the items of the array to data type number and console each items data type, use any of the array iteration methods provided by JS for iteration
 
 // why the variable is not declared?
 
-array.forEach(function (value) {
-    dataConversion = parseInt(value);
+numbers.forEach(function (value) {
+    let dataConversion = parseInt(value);
     console.log(`${value}  ${typeof dataConversion}`);
 });
 
@@ -22,39 +22,35 @@ array.forEach(function (value) {
 
 // can we use arrray.push here?
 
-array.splice(-3, 3);
-console.log(array);
-array.splice(0, 0, 'one', 'two');
-console.log(array);
+numbers.splice(-3, 3);
+console.log(numbers);
+numbers.unshift('one', 'two'); 
+console.log(numbers);
 
 // d. Using any one of the array iteration methods console the string concatenation of all items of the array and also console the sum of all the items ( convert to integer before calculating)
 
 // what is this? the o/p is not correct, havent u checked?
-
-var n = array.length;
-for (var i = 0; i < 1; i++) {
-    concatElements = '';
-    var concatElements =
-        array[i] + array[i + 1] + array[i + 2] + array[i + 3] + array[i + 4];
-    console.log(concatElements);
+let sumArray = ["1", "2", "3", "4", "5", "6", "7"] ;
+console.log(`sumarray: ${sumArray}`);
+let concatElements = "";
+let sum = 0;
+for ( let i=0; i<sumArray.length; i++){
+    concatElements += sumArray[i];
+    sum += parseInt(sumArray[i]);
 }
-
-var sum = 0;
-array.forEach(function (value) {
-    sum += parseInt(value);
-});
-console.log(`sum is ${sum}`);
+console.log("concatenated elements :" + concatElements);
+console.log(`sum ${sum}`);
 
 // e. Filter out item "3" from the array and console the array (use any array method)
 
-let filterArray = array.filter(function (value) {
+let filterArray = numbers.filter(function (value) {
     return value == 3;
 });
 console.log(`filtered array ${filterArray}`);
 
 // f. Iterate the array and console the item, when item is either "3", "6" or "7"
 
-array.forEach(function (value) {
+numbers.forEach(function (value) {
     if (value == '3' || value == '6' || value == '7') {
         console.log(value);
     }
@@ -66,14 +62,17 @@ array.forEach(function (value) {
 //[1, 2, "3", 4, 5, 6,"7"]  Compare this array with the above given here which u have compared?
 //(Compare each item of this array with each item of the other array)
 
-var array1 = ['1', '2', '3', '4', '5', '6', '7'];
-var array2 = [1, 2, '3', 4, 5, 6, '7'];
+// I have to redeclared the same array, since the array's value is changed.
+let array1= ['1', '2', '3', '4', '5', '6', '7'];
+let array2 = [1, 2, '3', 4, 5, 6, '7'];
 array1.forEach &&
     array2.forEach(function (value, index) {
         if (typeof array1[index] == typeof array2[index]) {
-            console.log(value);
+        let equalDataType = array1[index];
+        console.log(`element with same datatype: ${equalDataType}`);
         }
     });
+
 
 // h. [0,2,3,7,5,6,8] iterate the array and multiply each item by its index value and console the result only if result is greater than 40.
 
@@ -81,9 +80,9 @@ array1.forEach &&
 
 var array3 = [0, 2, 3, 7, 5, 6, 8];
 array3.forEach(function (value, index) {
-    multiple = value * index;
+    let multiple = value * index;
     if (multiple > 40) {
-        console.log(`${value} ${multiple}`);
+        console.log(`value: ${value} , multiple: ${multiple}`);
     }
 });
 
@@ -92,10 +91,10 @@ array3.forEach(function (value, index) {
 var array4 = [1, 2, 3, 4, 5];
 var array5 = [6, 7, 8, 9, 10];
 var mergeArray = array4.concat(array5);
-console.log(mergeArray);
+console.log(`merged array : ${mergeArray}`);
 
-// 2)​Console this star pattern
-
+// 2)
+// console this star pattern
 let l = 6;
 let pyramid = '';
 for (let i = 1; i <= l; i++) {
